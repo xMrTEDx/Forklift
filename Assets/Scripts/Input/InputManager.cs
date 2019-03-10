32b64adfbe9fd388843f4864fc56123c191f07c6
Adrian Probierz <xmrtedx@gmail.com>
2019-03-10 16:53:17 +0100

    InputManager

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InputManager : MonoBehaviour {
+	[Header("Przyciski")]
+    public List<InputButton> przyciski;
+
+
+    public enum Akcje{
+        interakcja,
+        menu,
+        skok,
+        bieg
+    }
+
+    [System.Serializable]
+    public class InputButton
+    {
+        public Akcje akcja;
+        public List<KeyCode> przyciski;
+    }
+    
+
+
+    // [Header("Osie")]
+    // public InputAxis gaz;
+
+    // public enum Osie{
+    //     Gaz,
+    //     Hamulec
+    // }
+
+    //[System.Serializable]
+    // public class InputAxis
+    // {
+    //     public Osie os;
+    //     public List<string> osie;
+    // }
+}
+
